e44b394862ef37e92d5a9f19b23a0b1129fd0616
Francisco Kim <ratonwasher@gmail.com>
2022-04-29 10:09:22 +0200

    Modify build.sh to first clean publish directory

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+# Remove contents of public directory
+if [ -d "public" ]
+then
+    if [ "$(ls -A public)" ]; then
+        rm -r public/*
+        echo "Directory \"public\" successfully emptied."
+    else
+        echo "Directory \"public\" is already empty."
+    fi
+else
+    echo "Directory \"public\" not found."
+fi
+
+# Execute Emacs Lisp file to build site
 emacs -Q --script build-site.el
-  
+
